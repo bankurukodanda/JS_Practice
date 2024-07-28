e2ca2e55e9ee8f070184bf99d58806c5cdea956d
Kodanda <kodandasurya.bankuru@servicenow.com>
2024-07-28 22:34:25 +0530

    Create Custom_bund_call_apply

---
+++ Custom_bund_call_apply.js
@@ -0,0 +1,28 @@
+//Custom Bind
+
+Function.prototype.myBind = function(...args){
+ let obj = this;
+ return function (...args2) {
+   obj.apply(args[0], [...args.slice(1), ...args2]);
+ }
+}
+
+//Custom Apply
+
+Function.prototype.myApply = function(...args){
+ var context = args[0];
+ 
+ context.fn = this;
+ context.fn(...args[1]);
+
+}
+
+//Custom Call
+
+Function.prototype.myApply = function(...args){
+ var context = args[0];
+ 
+ context.fn = this;
+ context.fn(...args.slice(1));
+
+}
