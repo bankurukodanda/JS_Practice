c2db83db819c2cfdb79f0556ffa8ba82920ba36b
Kodanda <b.suryaashok@gmail.com>
2025-12-13 22:32:31 +0530

    Create RacePromise.js

---
+++ RacePromise.js
@@ -0,0 +1,44 @@
+var test1 = function(){
+    return new Promise((res, rej)=>{
+        setTimeout(()=>{res("test1");},5000) 
+    })
+}
+var test2 = function(){
+    return new Promise((res, rej)=>{
+        setTimeout(()=>{res("test2");},1000) 
+    })
+}
+var test3 = function(){
+    return new Promise((res, rej)=>{
+        setTimeout(()=>{res("test3");},3000) 
+    })
+}
+var promises = [test1, test2, test3]
+function racePromise(promises){
+    return new Promise((res, rej)=>{
+        var count =0;
+        var results = [];
+        for(let i=0; i< promises.length; i++){
+            let rescount =0;
+            promises[i]().then((result)=>{
+                count++;
+                results.push({"ind":i, "res": result})
+                res(results);
+                
+            }).catch((error)=>{
+                count++;
+                results.push({"ind":i, "res": error})
+                if(count == promises.length){
+                    res(results);
+                }
+
+            })
+        }
+    })
+
+    
+}
+
+racePromise(promises).then(res=>{
+    console.log(res);
+})
